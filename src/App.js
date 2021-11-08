2e1d80e41d8b2df4f5bce65a4f55a0a753d6eac7
michaelraysor <michaelraysor1@gmail.com>
2021-11-08 18:42:09 -0500

    Updated css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
+import Footer from './Components/Footer';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App-header">
+    <p>
+    <ul>
+    <li>Trained & Certified</li>
+    <li>Mobile Notary</li>
+    <li>Loan Signing Agent</li>
+    <li>E-Notary</li>
+    </ul>
+    </p>
+      <h1 className="name"> A. R. Signing Services</h1>
+      <img className="picture1" alt="pic" src="https://www.nationalnotary.org/Image%20Library/NNA/OpenGraph.jpg"></img>
+      <img className="picture1" alt="pic" src="https://notarypublicunderwriters.com/images/virtuemart/product/100k-notary-insurance.jpg"></img>
+      <div>
+      <Footer />
+      </div>
+      </div>
+     
+
+      
   );
 }
 
---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ".//Footer.css";
+
+
+function Footer() {
+    return (
+        <div className="main-footer">
+            <h4>Contact Info</h4>
+        </div>
+    )
+}
+
+export default Footer;
+
