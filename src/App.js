6db3ad4cd66c94a279e39228823b5276b86f7f8c
michaelraysor <michaelraysor1@gmail.com>
2021-11-09 19:37:47 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import Footer from './Components/Footer';
 import './App.css';
+import Main from "./Components/Main";
 
 
 
 function App() {
   return (
-    <div className="App-header">
-    <p>
-    <ul>
-    <li>Trained & Certified</li>
-    <li>Mobile Notary</li>
-    <li>Loan Signing Agent</li>
-    <li>E-Notary</li>
-    </ul>
-    </p>
-      <h1 className="name"> A. R. Signing Services</h1>
-      <img className="picture1" alt="pic" src="https://www.nationalnotary.org/Image%20Library/NNA/OpenGraph.jpg"></img>
-      <img className="picture1" alt="pic" src="https://notarypublicunderwriters.com/images/virtuemart/product/100k-notary-insurance.jpg"></img>
-      <br/>
+    <div className="container1">
+    <div className="header-name">
+    <h1 className="name"> A. R. Signing Services</h1>
+    </div>
+    <div className="header-photos">
+    </div>
+     
+      <div className="content-wrap-center">
+      <Main />
+      </div>
      
     
     <div className="page-container">
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ".//Footer.css";
-
+import EmailIcon from '@mui/icons-material/Email';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 
 function Footer() {
     return (
@@ -8,8 +10,26 @@ function Footer() {
         <div className="container">
         <div className="row">
         <br />
-         <h4>Contact | Yelp | Phone</h4>
-        
+         
+
+               <div className="header-photos">
+               <img className="picture1" alt="pic" src="https://www.notary2notary.com/wp-content/uploads/2021/10/nna-services-nna-logo.jpg"></img>
+               <img className="picture1" alt="pic" src="https://notarypublicunderwriters.com/images/virtuemart/product/100k-notary-insurance.jpg"></img>
+               <img className="picture1" alt="pic" src="https://1000logos.net/wp-content/uploads/2018/03/Yelp-Logo-2004.png"></img>
+               <img className="picture1" alt="pic" src="https://www.notary2notary.com/wp-content/uploads/2021/09/Copy-of-Logos.png"></img>
+              
+               
+               
+               <div className="footer-hours">
+               <p>Hours of Operation</p> 
+               <h4>9am-3pm</h4>
+               <div className="icons">
+               <EmailIcon /> | <InstagramIcon /> | <LocalPhoneIcon /> | 
+               </div>
+               </div>
+               
+               </div>
+         
         </div>
         </div>
         </div>
