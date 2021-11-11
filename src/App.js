35cf1b495cf75142fc8589a7f3eacc2caca7b01e
michaelraysor <michaelraysor1@gmail.com>
2021-11-10 19:37:40 -0500

    Updated css

--- src/App.js
+++ src/App.js
@@ -7,9 +7,17 @@ import Main from "./Components/Main";
 
 function App() {
   return (
+    <div>
+    <div className="header-photos">
+    <h1 className="name1"> A. R. Signing Services</h1>
+   
+               <img className="picture1" alt="pic" src="https://www.notary2notary.com/wp-content/uploads/2021/10/nna-services-nna-logo.jpg"></img>
+               <img className="picture1" alt="pic" src="https://notarypublicunderwriters.com/images/virtuemart/product/100k-notary-insurance.jpg"></img>
+               </div>
     <div className="container1">
-    <div className="header-name">
-    <h1 className="name"> A. R. Signing Services</h1>
+    
+    <div className="maincenter">
+    
     </div>
     <div className="header-photos">
     </div>
@@ -25,7 +33,7 @@ function App() {
     <Footer />
     </div>
     </div>
-
+    </div>
       
       
      
